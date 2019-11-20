a7059ac31898d6f553eb7681564187fcb465560d
Vinod Selvin <vinod.selvin@switchme.in>
2019-11-20 16:03:22 +0530

    Bug of Get all headers bug fixed

--- System/Router.php
+++ System/Router.php
@@ -40,11 +40,25 @@ Class Router extends FrameworkV{
         }
         
         $this->request['uri_segments'] = $request_uri_segments;
-        $this->request['request_headers'] = getallheaders();
+        $this->request['request_headers'] = $this->getallheaders();
         
         $this->setController();
         
         return;
+    }
+
+    
+    function getallheaders() {
+	    
+	$headers = [];
+	
+	foreach ($_SERVER as $name => $value) {
+        	if (substr($name, 0, 5) == 'HTTP_') {
+            		$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+        	}
+	}
+
+    	return $headers;
     }
     
     function setController(){
